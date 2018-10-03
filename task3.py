95001b5d9034e090e69dc76af4fa1b5ea6ac87f7
ManjotKaur16 <43748000+ManjotKaur16@users.noreply.github.com>
2018-10-02 21:28:58 -0300

    Update task3.py

--- task3.py
+++ task3.py
@@ -5,7 +5,7 @@ def access_lst(file):
 		line=line.strip()
 		for i in line.split():
 			if i=='global_access' or i=='fw-management_access_in':
-                 a.append(line)
+                            a.append(line)
 
 	return a
 print(access_lst('running-config.cfg'))
